a3d45b9daf7cf9e79de12a4653cb641ea64ddaa4
panj <catifish@163.com>
2018-11-09 20:43:40 +0800

    add date to alert

--- watch.sh
+++ watch.sh
@@ -13,7 +13,7 @@ do
     percent=`df -h | grep $watch | awk '{print $5}'`
     percent=${percent:0:${#percent}-1}
     if [ $percent -gt 80 ]; then
-        (echo "硬盘${watch}当前占用${percent}%,超过警戒线${warningLimit}%"; sleep 1) | telnet 192.168.119.187 9999 > /dev/null 2>&1
+        (echo "[`date`]硬盘${watch}当前占用${percent}%,超过警戒线${warningLimit}%"; sleep 1) | telnet 192.168.119.187 9999 > /dev/null 2>&1
     fi
     sleep $interval
 done
