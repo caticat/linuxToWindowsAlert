1a933063172bfe71ae181d2cf83269e2ead75552
panj <catifish@163.com>
2018-11-09 20:53:49 +0800

    add new param to shell

--- watch.sh
+++ watch.sh
@@ -6,14 +6,16 @@
 watch="/dev/sda2" # 监听硬盘盘符
 warningLimit=80 # 触发监听百分比
 interval=$[60*60] # 监控间隔
+ip="192.168.119.187"
+port=9999
 
 # 处理
 while true
 do
     percent=`df -h | grep $watch | awk '{print $5}'`
     percent=${percent:0:${#percent}-1}
-    if [ $percent -gt 80 ]; then
-        (echo "[`date`]硬盘${watch}当前占用${percent}%,超过警戒线${warningLimit}%"; sleep 1) | telnet 192.168.119.187 9999 > /dev/null 2>&1
+    if [ $percent -gt $warningLimit ]; then
+        (echo "[`date`]硬盘${watch}当前占用${percent}%,超过警戒线${warningLimit}%"; sleep 1) | telnet $ip $port > /dev/null 2>&1
     fi
     sleep $interval
 done
