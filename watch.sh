85e79541bc5b5d7ab99889391264d72605ddf567
panj <catifish@163.com>
2018-11-10 17:16:19 +0800

    prevent same alarm to server

--- watch.sh
+++ watch.sh
@@ -10,12 +10,16 @@ ip="192.168.119.187"
 port=9999
 
 # 处理
+lastWarningPercent=0 # 保证不会因为同一个百分比连续不断的弹框
 while true
 do
     percent=`df -h | grep $watch | awk '{print $5}'`
     percent=${percent:0:${#percent}-1}
     if [ $percent -gt $warningLimit ]; then
-        (echo "[`date`]硬盘${watch}当前占用${percent}%,超过警戒线${warningLimit}%"; sleep 1) | telnet $ip $port > /dev/null 2>&1
+    	if [ $percent -gt $lastWarningPercent ]; then
+        	(echo "[`date`]硬盘${watch}当前占用${percent}%,超过警戒线${warningLimit}%"; sleep 1) | telnet $ip $port > /dev/null 2>&1
+        fi
     fi
+    lastWarningPercent=$percent
     sleep $interval
 done
